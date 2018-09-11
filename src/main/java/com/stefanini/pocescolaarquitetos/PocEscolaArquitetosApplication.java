9eccfb3a726b8fddc9dce198d4511324b1991471
wmcosta <wmcosta@latam.stefanini.com>
2018-09-11 16:41:46 -0300

    arcabouço do projeto

---
+++ src/main/java/com/stefanini/pocescolaarquitetos/PocEscolaArquitetosApplication.java
@@ -0,0 +1,12 @@
+package com.stefanini.pocescolaarquitetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocEscolaArquitetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocEscolaArquitetosApplication.class, args);
+	}
+}
