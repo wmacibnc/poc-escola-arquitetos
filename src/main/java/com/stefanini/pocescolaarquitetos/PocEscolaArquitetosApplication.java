64b26771d76f90b8012c26a1f2a371ba1f25ba0c
wmacibnc <wmacibnc@gmail.com>
2018-09-13 06:21:43 -0300

    Incluindo anotação para a aplicação ser um servidor de configuração

--- src/main/java/com/stefanini/pocescolaarquitetos/PocEscolaArquitetosApplication.java
+++ src/main/java/com/stefanini/pocescolaarquitetos/PocEscolaArquitetosApplication.java
@@ -1,9 +1,11 @@
 package com.stefanini.pocescolaarquitetos;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PocEscolaArquitetosApplication {
 
 	public static void main(String[] args) {
